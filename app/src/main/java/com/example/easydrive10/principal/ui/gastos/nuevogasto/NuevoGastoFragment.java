54daccf1a748c84dc057b9d8afb4c9393bfcd240
Houssam <khllou@hotmail.fr>
2020-06-10 19:40:49 +0200

    insertar gastos

--- app/src/main/java/com/example/easydrive10/principal/ui/gastos/nuevogasto/NuevoGastoFragment.java
+++ app/src/main/java/com/example/easydrive10/principal/ui/gastos/nuevogasto/NuevoGastoFragment.java
@@ -16,18 +16,36 @@ import androidx.fragment.app.Fragment;
 public class NuevoGastoFragment extends Fragment implements INuevoGastoInterface {
     private NuevoGastoViewModel nuevoGastoViewModel;
     private FragmentNuevoGastoBinding binding;
-    private Double totalCombustible;
+    private Double totalGastos, totalCombustible, totalPeajes, totalComida, totalOtrosGastos;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_nuevo_gasto, container, false);
         View view = binding.getRoot();
+        totalGastos = 0.0;
         totalCombustible = 0.0;
+        totalPeajes = 0.0;
+        totalComida = 0.0;
+        totalOtrosGastos = 0.0;
+////        CALCULAR COMBUSTIBLE
+//        calcular(binding.btnSumarCombustible, binding.txtValorCombustible);
+//        total(binding.btnTotalCombustible, binding.txtTotalCombustible);
+////        CALCULAR PEAJES
+//        calcular(binding.btnSumarPeajes, binding.txtValorPeajes);
+//        total(binding.btnTotalPeajes, binding.txtTotalPeajes);
+////        CALCULAR COMIDA
+//        calcular(binding.btnSumarComida, binding.txtValorComida);
+//        total(binding.btnTotalComida, binding.txtTotalComida);
+////        CALCULAR OTROS GASTOS
+//        calcular(binding.btnSumarOtrosGastos, binding.txtValorOtrosGastos);
+//        total(binding.btnTotalOtrosGastos, binding.txtTotalOtrosGastos);
+
+//        CALCULAR COMBUSTIBLE
         binding.btnSumarCombustible.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(binding.txtValorCombustible.getText().toString().isEmpty()){
+                if (binding.txtValorCombustible.getText().toString().isEmpty()) {
                     binding.txtValorCombustible.setText("0.0");
                 }
                 totalCombustible += Double.valueOf(binding.txtValorCombustible.getText().toString());
@@ -37,7 +55,59 @@ public class NuevoGastoFragment extends Fragment implements INuevoGastoInterface
         binding.btnTotalCombustible.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                binding.txtTotalCombustible.setText(totalCombustible.toString());
+                binding.txtTotalCombustible.setText(totalCombustible.toString() + "€");
+            }
+        });
+
+//        CALCULAR PEAJES
+        binding.btnSumarPeajes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (binding.txtValorPeajes.getText().toString().isEmpty()) {
+                    binding.txtValorPeajes.setText("0.0");
+                }
+                totalPeajes += Double.valueOf(binding.txtValorPeajes.getText().toString());
+                binding.txtValorPeajes.setText("");
+            }
+        });
+        binding.btnTotalPeajes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.txtTotalPeajes.setText(totalPeajes.toString() + "€");
+            }
+        });
+//        CALCULAR COMIDA
+        binding.btnSumarComida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (binding.txtValorComida.getText().toString().isEmpty()) {
+                    binding.txtValorComida.setText("0.0");
+                }
+                totalComida += Double.valueOf(binding.txtValorComida.getText().toString());
+                binding.txtValorComida.setText("");
+            }
+        });
+        binding.btnTotalComida.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.txtTotalComida.setText(totalComida.toString() + "€");
+            }
+        });
+//        CALCULAR OTROS GASTOS
+        binding.btnSumarOtrosGastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (binding.txtValorOtrosGastos.getText().toString().isEmpty()) {
+                    binding.txtValorOtrosGastos.setText("0.0");
+                }
+                totalOtrosGastos += Double.valueOf(binding.txtValorOtrosGastos.getText().toString());
+                binding.txtValorOtrosGastos.setText("");
+            }
+        });
+        binding.btnTotalOtrosGastos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.txtTotalOtrosGastos.setText(totalOtrosGastos.toString() + "€");
             }
         });
         binding.setLifecycleOwner(this);
@@ -49,4 +119,48 @@ public class NuevoGastoFragment extends Fragment implements INuevoGastoInterface
         SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences("preferenciasUsuario", Context.MODE_PRIVATE);
         String correoUsuario = sharedPreferences.getString("correo", "prueba@gmail.com");
     }
+
+//    public void calcular(ImageButton imageButton, final EditText editText) {
+//        imageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (editText.getText().toString().isEmpty()) {
+//                    editText.setText("0.0");
+//                }
+//                if (editText.getId() == R.id.txtValorCombustible) {
+//                    totalCombustible += Double.valueOf(editText.getText().toString());
+//                }
+//                if (editText.getId() == R.id.txtValorPeajes) {
+//                    totalPeajes += Double.valueOf(editText.getText().toString());
+//                }
+//                if (editText.getId() == R.id.txtValorComida) {
+//                    totalComida += Double.valueOf(editText.getText().toString());
+//                }
+//                if (editText.getId() == R.id.txtValorOtrosGastos) {
+//                    totalOtrosGastos += Double.valueOf(editText.getText().toString());
+//                }
+//                editText.setText("");
+//            }
+//        });
+//    }
+//
+//    public void total(ImageButton imageButton, final TextView textView) {
+//        imageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (textView.getId() == R.id.txtValorCombustible) {
+//                    textView.setText(totalCombustible.toString() + "€");
+//                }
+//                if (textView.getId() == R.id.txtValorPeajes) {
+//                    textView.setText(totalPeajes.toString() + "€");
+//                }
+//                if (textView.getId() == R.id.txtValorComida) {
+//                    textView.setText(totalComida.toString() + "€");
+//                }
+//                if (textView.getId() == R.id.txtValorOtrosGastos) {
+//                    textView.setText(totalOtrosGastos.toString() + "€");
+//                }
+//            }
+//        });
+//    }
 }
